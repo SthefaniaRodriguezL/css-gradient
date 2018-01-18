11d5af7a4bfe34c14e7f1e594c9eebf33ac298fa
JosephFernandez2689 <joe.fernandez2689@gmail.com>
2018-01-18 10:25:52 -0600

    fixing bugs worte code did not finish

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -5,7 +5,7 @@
  *
  */
 
-// EXAMPLE USAGE:
+EXAMPLE USAGE:
 // var list = new LinkedList();
 // list.tail;         //yields 'null'
 // list.addToTail(4);
@@ -20,22 +20,62 @@
 
 
 var LinkedList = function() {
-  //fill me in!
+  var list = {};
+  this.head = null;
+  this.tail = null;
+  var lastNode;
+    //fill me in!
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-) {
-};
+var LinkedList = function() {
+  var list = {};
+  this.head = null;
+  this.tail = null;
+  this.lastNode = 0;
 
+};
+LinkedList.prototype.addToTail = function(value) {
+
+  var currentNode = LinkedList.prototype.makeNode(value);
+
+  if (this.head === null) {
+    this.head = currentNode;
+    this.tail = currentNode;
+  } else {
+    this.lastNode = list.tail;
+    this.tail = currentNode;
+    this.lastNode.next = list.tail;
+  }
+};
 LinkedList.prototype.removeHead = function() {
-};
+  var storage = this.head.next;
+     var formerHead = this.head.value;
 
-LinkedList.prototype.contains = function(
-) {
+     delete this.head;
+     this.head = storage;
+     return formerHead;
+   };
+LinkedList.prototype.contains = function(target) {
+  var searchNode = function(target, node) {
+    if (node.value === target) {
+      return true;
+    } else if (node.next === null) {
+      return false;
+    } else {
+      return searchNode(target, node.next);
+    };
+    return searchNode(target, this.head);
+  };
+  return list;
 };
+LinkedList.prototype.makeNode = function(value) {
+  var node = {};
 
-LinkedList.prototype.makeNode = function(
-) {
+   node.value = value;
+   node.next = null;
+
+   return node;
+
 };
